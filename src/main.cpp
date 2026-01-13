4d9a3c9e4a30b54b2aac4e97fb873237f3a2f9c4
Michal Wittmayer <michal.wittmayer.025@pslib.cz>
2026-01-13 17:33:24 +0100

    Add LCD initialization and display function for menu options

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,31 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
+// Initialize the LCD with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+// LCD custom chars   
+byte arrowLeft[8] = {
+  B00010,
+  B00100,
+  B01100,
+  B11111,
+  B01100,
+  B00100,
+  B00010,
+  B00000
+};
+byte line[8] = {
+  B00000,
+  B00000,
+  B00000,
+  B11111,
+  B00000,
+  B00000,
+  B00000,
+  B00000
+};
+
 extern const char* menu125kHz[];
 extern const char* menu13MHz[];
 extern const char* menuIR[];
@@ -15,6 +40,15 @@ const char* mainMenu[] = {
   "Bad USB"
 };
 
+void showScreen(const char* line1, const char line2) {
+  // Function to display two lines on the LCD
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(line1);
+  lcd.setCursor(0, 1);
+  lcd.print(line2);
+}
 void setup() {
   // put your setup code here, to run once:
 }
