375f218e4879ddb54488b55afb18c0d75cb70761
Michal Wittmayer <michal.wittmayer.025@pslib.cz>
2026-01-14 16:45:05 +0100

    Added the LCD testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <LiquidCrystal.h>
 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
 void setup() {
     // Initialization code here
+    lcd.begin(16, 2);
+    lcd.clear();
+    lcd.print("Hello, World!");
 }
 
 void loop() {
