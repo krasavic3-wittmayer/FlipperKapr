dad75302212ca9a5a1817ff54bf35ef36b6020d2
Michal Wittmayer <michal.wittmayer.025@pslib.cz>
2026-01-14 17:55:58 +0100

    Finished the LCD testing (i think the display died)

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,11 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
     // Initialization code here
+    delay(1000);
     lcd.begin(16, 2);
     lcd.clear();
-    lcd.print("Hello, World!");
+    lcd.setCursor(0, 0);
+    lcd.print("TEST");
 }
 
 void loop() {
