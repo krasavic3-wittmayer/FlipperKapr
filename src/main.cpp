221d9d0132ce55ff81df859090869fde4168045f
Michal Wittmayer <michal.wittmayer.025@pslib.cz>
2026-01-15 19:32:40 +0100

    Tried to fix the RC522 module

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,30 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define SS_PIN 53
-#define RST_PIN 6   // ← tady musí sedět pin, kam jsi zapojil RST
+#define RST_PIN 49
+#define SS_PIN  53
 
-MFRC522 rfid(SS_PIN, RST_PIN);
+MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial);   // Mega tohle zvládne
+    Serial.begin(9600);
 
-  SPI.begin();
+    // POVINNÉ NA ARDUINO MEGA
+    pinMode(53, OUTPUT);       // HW SS MUSÍ být OUTPUT
+    digitalWrite(53, HIGH);
 
-  // Zpomalíme SPI – děliče to ocení
-  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
+    SPI.begin();
+    SPI.beginTransaction(SPISettings(500000, MSBFIRST, SPI_MODE0));
+    mfrc522.PCD_Init();
+    delay(4);
 
-  rfid.PCD_Init();
-  Serial.println("RC522 ready");
+    mfrc522.PCD_DumpVersionToSerial();
+    Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
 }
 
 void loop() {
-  // čekáme na kartu
-  if (!rfid.PICC_IsNewCardPresent()) return;
-  if (!rfid.PICC_ReadCardSerial()) return;
+    if (!mfrc522.PICC_IsNewCardPresent()) return;
+    if (!mfrc522.PICC_ReadCardSerial()) return;
 
-  Serial.print("UID: ");
-  for (byte i = 0; i < rfid.uid.size; i++) {
-    if (rfid.uid.uidByte[i] < 0x10) Serial.print("0");
-    Serial.print(rfid.uid.uidByte[i], HEX);
-    Serial.print(" ");
-  }
-  Serial.println();
-
-  rfid.PICC_HaltA();
-  rfid.PCD_StopCrypto1();
+    mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
 }
